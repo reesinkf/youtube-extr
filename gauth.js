d4152cd0ba5b65b4ebde51aec85905d0f1d92315
frans <frans@localhost.localdomain>
2018-07-02 16:44:54 +0200

    Testing spreadsheet creation

--- gauth.js
+++ gauth.js
@@ -16,16 +16,46 @@ const drive = google.drive({
 		
 	drive.files.create({
 		requestBody: {
-			name: 'Test',
+			name: 'Test 2',
 		    mimeType: 'text/plain'
 		},
 		media: {
 			mimeType: 'text/plain',
 		    body: 'Hello World'
 		}
-	}).then(function(file) {
-			console.log(file.data.id)
+	})
+
+
+	  var sheets = google.sheets('v4');
+  sheets.spreadsheets.create({
+    auth: auth,
+    resource: {
+        properties:{
+            title: "HUEAH"
+        }
+    }
+  		}, (err, response) => {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    } else {
+        console.log("Added");
+    }
+  	});
+
+		
+	drive.files.list({
+	  auth: auth,
+	  pageSize: 1000
+	}, function (err, resp) {
+	  if (err) {
+	    console.log("ERROR", err);
+	  }
+	  else {
+	    console.log(resp.data);
+	  }
 	});
+	
 
 
 
